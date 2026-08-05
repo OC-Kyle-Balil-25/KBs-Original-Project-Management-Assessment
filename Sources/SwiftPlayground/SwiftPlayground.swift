2d44f756133357aab0538cfc99707ea1d9e768f2
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-08-05 00:50:03 +0000

    Aug 5, 2026, 12:50 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,14 +7,19 @@ struct SwiftPlayground {
         var pointCount = 0
 
         let questionList = [
+            "What is the formula for finding the distance between two points",
+            "What is the formula for finding the gradient between two points",
+            "How do you find the negative reciprocal of a gradient?",
+
             "What is the Centroid?",
             "What is the Circumcentre?",
             "What is the Orthocentre?"
         ]
         let answerList = [
+            
             "Tri-intersection point of a triangle based on lines from midpoints to directly opposite vertexes",
             "Tri-intersection point of a triangle based on lines from straight perpendicular bisector midpoints",
-            "Tri-intersection point of a triangle based on lines from straight perpendicular bisectors to directly opposite vertexes",
+            "Tri-intersection point of a triangle based on lines from straight perpendicular bisectors to directly opposite vertexes"
         ]
     }
 }
