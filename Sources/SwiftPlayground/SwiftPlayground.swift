b55b6eea96b5153f294ba91e0f81b48ba38d7a5d
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-08-05 00:36:31 +0000

    Aug 5, 2026, 12:36 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -12,9 +12,9 @@ struct SwiftPlayground {
             "What is the Orthocentre?"
         ]
         let answerList = [
-            "Tri-intersection point of a triangle based on lines from vertexes to directly opposite midpoints",
-            "Tri-intersection point of a triangle based on lines from vertexes to directly opposite midpoints",
-            "Tri-intersection point of a triangle based on lines from vertexes to directly opposite midpoints",
+            "Tri-intersection point of a triangle based on lines from directly opposite midpoints to vertexes",
+            "Tri-intersection point of a triangle based on lines from directly opposite midpoints to vertexes",
+            "Tri-intersection point of a triangle based on lines from directly opposite midpoints to vertexes"
         ]
     }
 }
