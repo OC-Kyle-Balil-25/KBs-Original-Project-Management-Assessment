0eca4144e07396f10ccf7cd3bec3b180819abb0a
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-08-07 03:10:24 +0000

    7/8/2026: Sprint 2 - Day 2 (Question Categorization)

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -11,7 +11,6 @@ struct SwiftPlayground {
             "What is the formula for finding the gradient between two points?",
             "What is the formula for finding the midpoint between two points?",
             "What is the equation of a linear line?",
-
             "How do you find the negative reciprocal of a gradient?",
 
             "What is the Centroid?",
@@ -19,24 +18,28 @@ struct SwiftPlayground {
             "What is the Orthocentre?"
         ]
         var questionListRandom = questionList
-        var question = ""
 
         var answerList = [
             "(x2-x1, y2-y1)",
             "(y2-y1)/(x2-x1)",
             "(x1+x2, y1+y2)/2",
             "y=mx+c",
-
             "(a/b -> -a/b or a/-b)",
 
-            "Tri-intersection point of a triangle based on lines from midpoints to directly opposite vertexes",
-            "Tri-intersection point of a triangle based on lines from straight perpendicular bisector midpoints",
-            "Tri-intersection point of a triangle based on lines from straight perpendicular bisectors to directly opposite vertexes",
+            "Tri-intersection point of a triangle based on lines from midpoints to directly opposite vertices",
+            "Tri-intersection point of a triangle based on lines from perpendicular bisector midpoints",
+            "Tri-intersection point of a triangle based on lines from perpendicular bisectors to directly opposite vertices",
         ]
 
         questionListRandom.shuffle()
-        for question in questionListRandom {
-            if question = questionList[1]
+        for questionConst in questionListRandom {
+            // Converts the question string from a constant to a variable
+            var questionVar = questionConst
+            if questionVar == questionList[0] || questionVar == questionList[1] || questionVar == questionList[2] || questionVar == questionList[3] || questionVar == questionList[4] {
+                print ("Formulae: \(questionVar)")
+            } else {
+                print ("Triangular Center: \(questionVar)")
+            }
         }
     }
 }
