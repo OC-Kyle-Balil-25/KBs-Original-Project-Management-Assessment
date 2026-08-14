e7c3b764e2ba65502ff11f67b7529026c3ce114f
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-08-14 10:54:19 +0000

    14/8/2026: Sprint 3 - Day 2 (Other Components (Part 2))

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,10 +4,14 @@
 @main
         struct SwiftPlayground {
                 static func main() {
-// General Variables
+//// General Variables
 
-var pointCount = 0
+/// Increases by 1 for every looparound to progress certain variables in said loops
+var loopScroller = 0
 
+var scoreStat = 0
+
+/// Fixed list of questions
 var questionList = [
     "What is the formula for finding the distance between two points?",
     "What is the formula for finding the gradient between two points?",
@@ -19,8 +23,10 @@ var questionList = [
     "What is the Circumcentre?",
     "What is the Orthocentre?"
 ]
-var questionListRandom = questionList
+/// Copy of questionList, but shuffled
+var questionListRandom = questionList.shuffle()
 
+/// Fixed list of answers
 var answerList = [
     "(x2-x1, y2-y1)",
     "(y2-y1)/(x2-x1)",
@@ -32,36 +38,45 @@ var answerList = [
     "Tri-intersection point of a triangle based on lines from perpendicular bisector midpoints",
     "Tri-intersection point of a triangle based on lines from perpendicular bisectors to directly opposite vertices",
 ]
-var loopScroller = 0
 
 /*
 Provides a list of three cateredly order-randomized answers.
 */
 // func randomizeAnswers() {
+//     // Shuffles the given group of answers
 //     answerRange.shuffle()
+//     /// Placeholder array to hold three given answers.
 //     var randomAnswers: [String] = ["","",""]
-//     for randomAnswer in 0..<2 {
+//     // Scrolls through each empty choice in randomAnswers
+//     for randomAnswer in randomAnswers {
+//     // Pulls out a random answer from answerRange and inserts it into the respective index of the current looparound
 //     randomAnswers[loopScroller] = answerRange.randomElement()!
+//     // Prepares loopScroller for next looparound
 //     loopScroller += 1
 //     }
+//     // Prints out the three answers in a numbered lsit
 //     print (randomAnswers)
 // }
 
+///// Holds one of two categories of unedited answers. Varred here with placeholder content to allow function recognition
 // var answerRange = ["1","2","3"]
-// questionListRandom.shuffle()
+// Goes through every question in the quiz
 // for questionConst in questionListRandom {
 //     // Converts the question string from a constant to a variable
 //     var questionVar = questionConst
-//     // Randomizes the order from one of two fixed sets of answers (limited down to 3) based on the question provided
+//     // Randomizes the order from one of two fixed sets of answers (limited down to 3) based on the question provided. Ordered in reverse for efficiency.
 //     if questionVar == questionList[5] || questionVar == questionList[6] || questionVar == questionList[7] {
-//         var answerRange = Array(answerList[0...4])
+//         // Answer Category: Formulae
+//         var answerRange = Array(answerList[5...7])
 //         randomizeAnswers()
 //     } else {
-//         var answerRange = Array(answerList[5...7])
+//         // Answer Category: Triangular Center
+//         var answerRange = Array(answerList[0...4])
 //         randomizeAnswers()
 //     }
 // }
-// var xpMultiplier = stride(from: 0.25, through: 3, by: 0.25).randomElement()
-// print (xpMultiplier)
+
+var scoreMultiplier = Array(stride(from: 0.25, through: 3, by: 0.25)).randomElement()!
+print (scoreMultiplier)
                 }
         }
