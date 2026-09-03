cc8d69f20ff4ac20c2d58c388b6502b2c5f338f8
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-09-03 20:52:11 +0000

    Sep 3, 2026, 8:52 PM

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftPlayground",
+    dependencies: [
+        .package(
+        url: "https://github.com/SwiftTUI/swift-tui",
+        .upToNextMinor(from: "0.9.11")
+    ),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
