97415c8ca336cb5cc12616a8d330cd0012dc7309
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-09-14 13:02:20 +0000

    Sep 14, 2026, 1:02 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import Foundation
 import SwiftTUI
 
@@ -11,90 +8,155 @@ struct SwiftPlayground: App {
             TripleChoiceQuiz()
         }
     }
+}
 
-    static func main() {
-        //// General Variables
-        /// Increases by 1 numeric unit for every looparound to progress numeric displays in Question Category Picker for_in_ Loop. Always scrolls eight times.
-        var eightScroller = 0
-        /// Increases by 1 numeric unit for every looparound to progress letterList to display in Answer Shuffler for_in_ Loop. Always scrolls three times.
-        var threeScroller = 0
-        /// List of letters in order to list answers.
-        var letterList = ["A", "B", "C"]
+struct TripleChoiceQuiz: View {
 
-        var scoreStat = 0
+/* Points System */
+/// Total Sum/Product of Points Accumulated/Decumulated.
+@State private var pointTotal: Int = 0
+/// Displays and Confirms Answer validity including total number of Points having been affected by button responses, in TERMINAL.
+@State private var pointDisplay: String = "Points: 0"
 
-        /// Fixed list of questions.
-        var questionList = [
-            "What is the formula for finding the distance between two points?",
-            "What is the formula for finding the gradient between two points?",
-            "What is the formula for finding the midpoint between two points?",
-            "What is the equation of a linear line?",
-            "How do you find the negative reciprocal of a gradient?",
+/* Quiz Progress State */
+/// Index Number of Current Question & indirectly, Correct Answer.
+@State private var currentListIndex: Int = 0
+/// Shuffled List of 8 Indexes to Shuffle Order of Connected Questions and Correct Answers.
+@State private var randomIndexOrder: [Int] = Array(0..<8).shuffled()
 
-            "What is the Centroid?",
-            "What is the Circumcentre?",
-            "What is the Orthocentre?",
-        ]
-        /// Copy of questionList, but shuffled.
-        var questionListRandom = questionList.shuffled()
+/* Current Display State */
+/// Displays Current Question in TERMINAL.
+@State private var currentQuestionDisplay: String = "Loading Coordinationally Geometric Knowledge..."
+/// List of Three Shuffled Answers.
+@State private var currentAnswerList: [String] = []
+/// Correct Answer Respective to currentQuestionDisplay.
+@State private var correctAnswer: String = ""
 
-        /// Fixed list of answers.
-        var answerList = [
-            "(x2-x1, y2-y1)",
-            "(y2-y1)/(x2-x1)",
-            "(x1+x2, y1+y2)/2",
-            "y=mx+c",
-            "(a/b -> -a/b or a/-b)",
+/* 0-4: Formulae | 5-7: Triangular Centre */
+/// Fixed list of questions.
+let questionList = [
+    "What is the formula for finding the distance between two points?",
+    "What is the formula for finding the gradient between two points?",
+    "What is the formula for finding the midpoint between two points?",
+    "What is the equation of a linear line?",
+    "How do you find the negative reciprocal of a gradient?",
+    "What is the Centroid?",
+    "What is the Circumcentre?",
+    "What is the Orthocentre?"
+]
+/// Fixed list of answers.
+let answerList = [
+    "(x2-x1, y2-y1)",
+    "(y2-y1)/(x2-x1)",
+    "(x1+x2, y1+y2)/2",
+    "y=mx+c",
+    "(a/b -> -a/b or a/-b)",
+    "Tri-intersection point of a triangle based on lines from midpoints to directly opposite vertices",
+    "Tri-intersection point of a triangle based on lines from perpendicular bisector midpoints",
+    "Tri-intersection point of a triangle based on lines from perpendicular bisectors to directly opposite vertices"
+]
 
-            "Tri-intersection point of a triangle based on lines from midpoints to directly opposite vertices",
-            "Tri-intersection point of a triangle based on lines from perpendicular bisector midpoints",
-            "Tri-intersection point of a triangle based on lines from perpendicular bisectors to directly opposite vertices",
-        ]
-
-        /*
-        Provides a list of three cateredly shuffled answers.
-*/
-        func randomizeAnswers(from minIndex: Int, to maxIndex: Int) {
-            // Shuffles the given group of answers.
-            answerRange = Array(answerList[minIndex...maxIndex]).shuffled()
-            /// Placeholder array to hold three random answers.
-            var randomAnswers: [String] = ["", "", ""]
-            // // Prints question.
-            // print("\(eightScroller). \(questionVar)")
-            // Scrolls through each empty choice in randomAnswers.
-            for randomAnswer in randomAnswers {
-                // Pulls out a random answer from answerRange and inserts it into the respective index of the current looparound.
-                randomAnswers[threeScroller] = answerRange[threeScroller]
-                // // Prints out the three answers in a lettered list
-                // print("\(letterList[threeScroller]). \(randomAnswers[threeScroller])")
-                // Prepares threeScroller for next looparound.
-                threeScroller += 1
-            }
-            // Resets threeScroller for next question looparound.
-            threeScroller = 0
-        }
-
-        /// Holds one of two categories of unedited answers. Varred here with placeholder content to allow function recognition.
-        var answerRange = ["1", "2", "3"]
-        /// Converts each question string from a constant to a variable. Varred here with placeholder content to allow function recognition.
-        var questionVar = "?"
-        // Goes through every question in the quiz.
-        for questionConst in questionListRandom {
-            //   Prepares eightScroller for printed display in randomizeAnswers.
-            eightScroller += 1
-            // Converts each question string from a constant to a variable.
-            questionVar = questionConst
-            // Randomizes the order from one of two fixed sets of answers (limited down to 3) based on the question provided. Ordered in reverse for efficiency.
-            if questionList[5...7].contains(questionVar) {
-                // Answer Category: Triangular Center
-                randomizeAnswers(from: 5, to: 7)
-            } else {
-                // Answer Category: Formulae
-                randomizeAnswers(from: 0, to: 4)
+/// Terminal Display
+var body: some View {
+    VStack(alignment: .center, spacing: 1) {
+        Text("--- \(currentQuestionDisplay) ---")
+        // Three Buttons containing one Answer each.
+        if currentAnswerList.count == 3 {
+            HStack(spacing: 2) {
+                Button(currentAnswerList[0]) {
+                    pickButton(containing: currentAnswerList[0])
+                }
+                Button(currentAnswerList[1]) {
+                    pickButton(containing: currentAnswerList[1])
+                }
+                Button(currentAnswerList[2]) {
+                    pickButton(containing: currentAnswerList[2])
+                }
             }
         }
-
-        // var scoreMultiplier = Array(stride(from: 0.25, through: 3, by: 0.25)).randomElement()!
-        // print (scoreMultiplier)
+        Text("--- \(pointDisplay) ---")
     }
+    // Repositions Button Contents further away from Button Edges.
+    .padding()
+    // Expands Borders to Centrally Encompass Full TERMINAL.
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    /// Displays first Question on Startup.
+    .onAppear {
+        nextQuestion()
+    }
+}
+
+/*
+: Checks Answer Validity when Respective Button Picked.
+: - Parametres:
+:   - currentAnswerSingle: Each Individual Answer from [currentAnswerList]
+*/
+func pickButton(containing currentAnswerSingle: String) {
+    if currentAnswerSingle == correctAnswer {
+        // Correct Answer adds a point.
+        pointTotal += 1
+        // Confirms Correctness of Answer with changed Point Total
+        pointDisplay = "Correct answer! You now have \(pointTotal) points!"
+    } else {
+        // Confirms Wrongness of Answer with unchanged Point Total
+        pointDisplay = "Wrong answer! You still have \(pointTotal) points!"
+    }
+    currentListIndex += 1
+    nextQuestion()
+}
+
+/*
+: Sets up Next Question, or displays Summary if Quiz has ended.
+*/
+func nextQuestion() {
+    // Checks if all Questions have been Answered.
+    if currentListIndex >= randomIndexOrder.count {
+        // Confirms and Displays Quiz Completion.
+        currentQuestionDisplay = "All questions answered!"
+        // Displays Final Score.
+        pointDisplay = "Your final score sums to \(pointTotal)/\(randomIndexOrder.count) points!"
+    } else {
+        /// Picks shuffled index from [randomIndexOrder], according to current knowledgeable content progression of quiz.
+        var randomListIndex = randomIndexOrder[currentListIndex]
+        // Picks Next Question using randomListIndex, for Next Question Display.
+        currentQuestionDisplay = questionList[randomListIndex]
+        // Sets up Correct Answer Connected to Next Question.
+        correctAnswer = answerList[randomListIndex]
+
+        /// Container for Three Answers before Shuffling.
+        var possibleAnswerList: [String] = ["Incorrect","Answers"]
+
+        if randomListIndex >= 5 && randomListIndex <= 7 {
+            /* Answer Category: Triangular Centre */
+            // Instant 3 Answers with only 1 always Correct and no Duplicates.
+            possibleAnswerList = Array(answerList[5...7])
+        } else {
+            /* Answer Category: Formulae */
+            // Container for more than Three Answers (Formulae Answer Category), to be cut down.
+            var otherAnswerList = Array(answerList[0...4])
+            // Removes Correct Answer Connected to Next Question.
+            otherAnswerList.removeAll {
+                $0 == correctAnswer
+            }
+            // Shuffles List of all Incorrect (Formulae) Answers.
+            otherAnswerList.shuffle()
+
+            /// Used to scroll through 2-Element Arrays.
+            var twoScroller = 0
+
+            /// Sets up 3-Answer (Formulae) List for Next Question.
+            for possibleAnswerSingle in possibleAnswerList {
+                // Adds 2 Incorrect Answers First
+                possibleAnswerList[twoScroller] = otherAnswerList[twoScroller]
+                // Prepares Loop to Collect Second Incorrect Answer.
+                twoScroller += 1
+            }
+            // Adds 1 Correct Answer Last.
+            possibleAnswerList.append(correctAnswer)
+        }
+        // Shuffles Complete Answer List for Maximum Non-Cheesability, for Next Question.
+        currentAnswerList = possibleAnswerList.shuffled()
+    }
+}
+    
 }
--- Sources/SwiftPlayground/TripleChoiceQuiz.swift
+++ Sources/SwiftPlayground/TripleChoiceQuiz.swift
@@ -1,39 +1,39 @@
-import SwiftTUI
+// import SwiftTUI
 
-struct TripleChoiceQuiz: View {
-@State private var points: Int = 0
-@State private var answerText: String = "Points: 0"
-@State public var questionConst: String = "Question:"
-@State public var randomAnswers: [String] = ["","",""]
-@State public var threeScroller: Int = 0
+// struct TripleChoiceQuiz: View {
+// @State private var points: Int = 0
+// @State private var answerText: String = "Points: 0"
+// @State public var questionConst: String = "Question:"
+// @State public var randomAnswers: [String] = ["","",""]
+// @State public var threeScroller: Int = 0
 
-    var body: some View {
-        VStack(alignment: .center, spacing: 1) {
-Text("--- \(questionConst) ---")
-HStack(spacing: 2) {
-    Button("randomAnswers[0])") {
-        points += 1
-        answerText = "Correct answer! You now have \(points) points!"
-    }
+//     var body: some View {
+//         VStack(alignment: .center, spacing: 1) {
+// Text("--- \(questionConst) ---")
+// HStack(spacing: 2) {
+//     Button("randomAnswers[0])") {
+//         points += 1
+//         answerText = "Correct answer! You now have \(points) points!"
+//     }
     
-    Button("randomAnswers[1])") {
-        answerText = "Wrong answer! You still have \(points) points!"
-    }
+//     Button("randomAnswers[1])") {
+//         answerText = "Wrong answer! You still have \(points) points!"
+//     }
     
-    Button("randomAnswers[2])") {
-        answerText = "Wrong answer! You still have \(points) points!"
-    }
-    for randomAnswer in randomAnswers {
-        Button(randomAnswers[threeScroller]) {
-        points += 1
-        answerText = "Correct answer! You now have \(points) points!"
-        }
-    }
-}
+//     Button("randomAnswers[2])") {
+//         answerText = "Wrong answer! You still have \(points) points!"
+//     }
+//     for randomAnswer in randomAnswers {
+//         Button(randomAnswers[threeScroller]) {
+//         points += 1
+//         answerText = "Correct answer! You now have \(points) points!"
+//         }
+//     }
+// }
 
-Text("--- \(answerText) ---")
-        }
-.padding()
-.frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
-}
+// Text("--- \(answerText) ---")
+//         }
+// .padding()
+// .frame(maxWidth: .infinity, maxHeight: .infinity)
+//     }
+// }
