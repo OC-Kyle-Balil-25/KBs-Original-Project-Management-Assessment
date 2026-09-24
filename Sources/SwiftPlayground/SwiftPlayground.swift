d3a330d22e8b7270ef40037b58ec96f75b36b30e
OC-Kyle-Balil-25 <kyle.balili@student.onslow.school.nz>
2026-09-24 06:03:18 +0000

    Sep 24, 2026, 6:03 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,26 +1,60 @@
+/// Imports Default Swift Code.
 import Foundation
+/// Imports Swift TERMINAL User Interface Code.
 import SwiftTUI
 
+/// Mains Code.
 @main
+/// Constructs Default Swift Code.
 struct SwiftPlayground: App {
+    /// Constructs TERMINAL.
     var body: some Scene {
         WindowGroup {
+            // Calls TERMINAL Content.
             TripleChoiceQuiz()
         }
     }
 }
 
+/// TERMINAL Content.
 struct TripleChoiceQuiz: View {
+//* TERMINAL CODE START *//
+
+/* Placeholder Variables */
+/// Total # of Answer Buttons.
+@State private var allAnswerButtons: Int = 3
+/// Increases certain variables by 1 for every Correct Answer.
+@State private var correctPoint: Int = 1
+/// Increases certain variables by 1 for every Question Answered.
+@State private var questionProgressor: Int = 1
+/// Null # of Multipliers.
+@State private var noMultipliers: Int = 0
+/// Minimum Size Multiplier.
+@State private var minMultiplier: Double = 0.25
+/// Maximum Size Multiplier.
+@State private var maxMultiplier: Double = 3.00
+/// Size Increments of Multiplier Range.
+@State private var multiplierIncrement: Double = 0.25
+/// Use of a Multiplier.
+@State private var multiplierUse: Int = 1
+/// Minimum Formulae Question.
+@State private var minQuestionForm: Int = 0
+/// Maximum Formulae Question.
+@State private var maxQuestionForm: Int = 4
+/// Minimum Triangular Centre Question.
+@State private var minQuestionTricent: Int = 5
+/// Maximum Triangular Centre Question.
+@State private var maxQuestionTricent: Int = 7
+/// Resets Scroller Variables.
+@State private var scrollerReset: Int = 0
+/// Compares Numbers Against Self for Pluralism.
+@State private var singlePlural: Double = 1.0
 
 /* Plural Variables */
 /// Single Letter to accurately follow single-letter pluralism when needed in TERMINAL.
 @State private var pluralS: String = ""
 /// "Be" Past-Tense to accurately follow "Be" Past-Tense pluralism when needed in TERMINAL.
 @State private var pluralWasWere: String = "was"
-/// Temporary debugging plural to display in TERMINAL.
-@State private var testPlural: String = ""
-/// Private copy of pluralS specifically for "multiplier/multipliers".
-var multiplierPlural = ""
 
 /* Points System */
 /// Total Sum/Product of Points Accumulated/Decumulated.
@@ -28,7 +62,7 @@ var multiplierPlural = ""
 /// Displays and Confirms Answer validity including total number of Points having been affected by button responses, in TERMINAL. Starts specifically as "Points: 0" because no Questions or Multipliers have been Answered/Used yet.
 @State private var pointDisplay: String = "Points: 0"
 /// Total Number of Questions Answered Correctly.
-@State private var correctTotal: Double = 0
+@State private var correctTotal: Int = 0
 
 /* Quiz Progress State */
 /// Index Number of Current Question & indirectly, Correct Answer.
@@ -37,10 +71,10 @@ var multiplierPlural = ""
 @State private var randomIndexOrder: [Int] = Array(0..<8).shuffled()
 
 /* Multiplier State */
-// Randomized multiplier to affect pointTotal. Starts displayed as 1 to show that pointTotal has not yet been significantly affected in TERMINAL.
-@State private var pointMultiplier: Double = 1
+// Randomized multiplier to affect the Point Total. Starts displayed as 1 to show that the Point Total has not yet been significantly affected in TERMINAL.
+@State private var pointMultiplier: Double = 1.0
 // Ensures multiplyPoints() can only be used once per Question on average.
-@State private var multiplierAttempts: Double = 0
+@State private var multiplierAttempts: Int = 0
 /// Actively displays total amount of Multipliers in TERMINAL.
 @State private var multiplierDisplay: String = "You have 0 Multipliers! Answer more questions to earn more Multipliers!"
 
@@ -88,7 +122,7 @@ var body: some View {
         // Displays Quiz Progress.
         Text("--- \(currentQuestionDisplay) ---")
         // Three Buttons containing one Answer each.
-        if currentAnswerList.count == 3 {
+        if currentAnswerList.count == allAnswerButtons {
             HStack(spacing: 2) {
                 for currentAnswer in currentAnswerList {
                     Button(currentAnswer) {
@@ -112,7 +146,7 @@ var body: some View {
     .padding()
     // Expands Borders to Centrally Encompass Full TERMINAL.
     .frame(maxWidth: .infinity, maxHeight: .infinity)
-    /// Displays first Question on Startup.
+    /// Displays First Question on Startup.
     .onAppear {
         nextQuestion()
     }
@@ -126,15 +160,17 @@ var body: some View {
 func pickAnswer(containing currentAnswerSingle: String) {
     if currentAnswerSingle == correctAnswer {
         // Correct Answer adds a point.
-        pointTotal += 1
-        correctTotal += 1
+        pointTotal += Double(correctPoint)
+        correctTotal += correctPoint
         // Confirms Correctness of Answer with changed Point Total.
         updatePoints(suffixing: "Correct answer! You now")
     } else {
         // Confirms Wrongness of Answer with unchanged Point Total.
         updatePoints(suffixing: "Wrong answer! you still")
     }
-    currentListIndex += 1
+    // Progresses Question after Current Answer Picked.
+    currentListIndex += questionProgressor
+    // Displays next Question after Current Answer Picked.
     nextQuestion()
 }
 
@@ -142,14 +178,15 @@ func pickAnswer(containing currentAnswerSingle: String) {
 : Multiplies Points by a random amount. Both decreasing and increasing Multipliers.
 */
 func multiplyPoints() {
-    if multiplierAttempts != 0 {
+    if multiplierAttempts != noMultipliers {
         // Randomizes multiplier from a range of 0.25-3.00, in increments of 0.25.
-        pointMultiplier = Array(stride(from: 0.25, through: 3, by: 0.25)).randomElement()!
-        // Multiplies current pointTotal.
+        pointMultiplier = Array(stride(from: minMultiplier, through: maxMultiplier, by: multiplierIncrement)).randomElement()!
+        // Privately copies pointTotal specifically for effective compatibility with pointMultiplier.
         pointTotal *= pointMultiplier
         // Updates Point Display to reflect Multiplier change.
         updatePoints(suffixing: "You now")
-        multiplierAttempts -= 1
+        // Subtracts Multiplier after use.
+        multiplierAttempts -= multiplierUse
     }
     // Adapts multiplierDisplay based on Subtraction of a Multiplier.
     updateMultipliers()
@@ -159,13 +196,14 @@ func multiplyPoints() {
 : Updates Points with a Reason Statement.
 : - Parametres:
 :   - reasonStatement: Reason prefixing how pointTotal was actively changed.
+:   - pointVariant: Chosen Variable Type to represent # of Points in pointDisplay.
 */
 func updatePoints(suffixing reasonStatement: String) {
     // Checks if pointTotal is numerically either singular or plural after Effect contextualized by reasonStatement.
     adaptPlural(using: pointTotal)
     /// Privately copies pluralS specifically to adapt "point/points".
     var pointPlural = pluralS
-    // Confirms reason of changed pointTotal all displayed in TERMINAL.
+    // Confirms reason of changed pointVariant all displayed in TERMINAL.
     pointDisplay = "\(reasonStatement) have \(pointTotal) point\(pointPlural)!"
 }
 
@@ -173,8 +211,10 @@ func updatePoints(suffixing reasonStatement: String) {
 : Updates Multiplier Attempt Total.
 */
 func updateMultipliers() {
+    /// Privately copies multiplierAttempts specifically for checking by adaptPlural().
+    var decimalMultiplierAttempts = Double(multiplierAttempts)
     /// Adapts pluralS based on Addition/Subtraction of a Multiplier.
-    adaptPlural(using: multiplierAttempts)
+    adaptPlural(using: decimalMultiplierAttempts)
     /// Privately copies pluralS specifically to adapt "multiplier/multipliers" after Addition/Subtraction of a Multiplier.
     var multiplierPlural = pluralS
     // Updates multiplierDisplay to reflect Multiplier Subtraction.
@@ -202,14 +242,14 @@ func nextQuestion() {
         /// Container for Three Answers before Shuffling.
         var possibleAnswerList: [String] = ["Incorrect","Answers"]
 
-        if randomListIndex >= 5 && randomListIndex <= 7  {
+        if randomListIndex >= minQuestionTricent && randomListIndex <= maxQuestionTricent {
             /* Answer Category: Triangular Centre */
             // Instant 3 Answers with only 1 always Correct and no Duplicates.
-            possibleAnswerList = Array(answerList[5...7])
+            possibleAnswerList = Array(answerList[minQuestionTricent...maxQuestionTricent])
         } else {
             /* Answer Category: Formulae */
-            // Container for more than Three Answers (Formulae Answer Category), to be cut down.
-            var otherAnswerList = Array(answerList[0...4])
+            /// Container for more than Three Answers (Formulae Answer Category), to be cut down.
+            var otherAnswerList = Array(answerList[minQuestionForm...maxQuestionForm])
             // Removes Correct Answer Connected to Next Question.
             otherAnswerList.removeAll {
                 $0 == correctAnswer
@@ -218,14 +258,14 @@ func nextQuestion() {
             otherAnswerList.shuffle()
 
             /// Used to scroll through 2-Element Arrays.
-            var twoScroller = 0
+            var twoScroller = scrollerReset
 
             /// Sets up 3-Answer (Formulae) List for Next Question.
             for possibleAnswerSingle in possibleAnswerList {
                 // Adds 2 Incorrect Answers First
                 possibleAnswerList[twoScroller] = otherAnswerList[twoScroller]
                 // Prepares Loop to Collect Second Incorrect Answer.
-                twoScroller += 1
+                twoScroller += questionProgressor
             }
             // Adds 1 Correct Answer Last.
             possibleAnswerList.append(correctAnswer)
@@ -234,7 +274,7 @@ func nextQuestion() {
         currentAnswerList = possibleAnswerList.shuffled()
 
         // Adds one Multiplier per Question Answered.
-        multiplierAttempts += 1
+        multiplierAttempts += questionProgressor
 
         // Adapts multiplierDisplay based on Subtraction of a Multiplier.
         updateMultipliers()
@@ -249,7 +289,7 @@ func nextQuestion() {
 */
 func adaptPlural(using numericUnit: Double) {
     // Tests selected number for pluralism.
-    if numericUnit == 1 {
+    if numericUnit == singlePlural {
         // Words should grammatically be singular.
         pluralS = ""
         pluralWasWere = "was"
@@ -259,5 +299,5 @@ func adaptPlural(using numericUnit: Double) {
         pluralWasWere = "were"
     }
 }
-
+//* TERMINAL CODE END *//
 }
